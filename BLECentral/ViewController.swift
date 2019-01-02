144bd4a42884998126599e97dcbb0787262b40db
Mikołaj Skawiński <mikolaj.skawinski@netguru.pl>
2019-01-02 13:25:51 +0100

    Initial Commit

---
+++ BLECentral/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLECentral
+//
+//  Created by Mikołaj Skawiński on 02/01/2019.
+//  Copyright © 2019 Mikołaj Skawiński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
